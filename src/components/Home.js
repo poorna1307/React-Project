e35af2cc53aa7b991856c5f7bfd3ce9cce43dfc4
Thumu Poorna Chander <poornachander.thumu13@gmail.com>
2022-07-04 23:01:00 +0530

    Home page modified

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,20 @@ function Home(){
             <h1 className="mainHeading text-center mt-3">Welcome to Tic-Tac-Toe</h1>
         </div>
         <div className="row text-center justify-content-around m-5">
-            <div className="col-lg-6 text-center">
+            <div className="col-lg-6 text-center p-5">
                 <img src={img1} alt="" className="w-75 rounded" />
             </div>
             <div className="col-lg-6 text-center">
                 <h3 className="mainHeading">Have Fun</h3>
-                <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia quod, quaerat mollitia, omnis ea iste maiores dicta earum debitis impedit fugiat quibusdam iure quos eveniet aut quas. Quia unde vitae provident modi dicta, veniam natus obcaecati assumenda ea commodi dolorum id saepe aut, deleniti eum quasi a ipsum eveniet rerum?</p>
+                <pre className="lead">Tic-Tac-Toe is a simple and fun game for 2 players, X and O. It is played on a 3x3 grid. Each player's goal is to make 3 in a row.
+
+Typically, X starts first, but in Gametable's Tabletop Tic Tac Toe, Player 1 starts first on the first game and Player 2 (or the computer) starts first on the next game. The starting player continues to alternate from game to game. This helps keep gameplay fair over time.
+
+Players take turns placing their Mark, X or O, on an open square in the grid. The first player to make 3 of their own mark in a row vertically, horizontally, or diagonally wins the game.
+
+If all 9 squares are filled and neither player has 3 in a row, the game is considered a Tie.
+
+The expert artificial intelligence (AI) will play a perfect game. Try your best to Tie it for as long as you can! </pre>
             </div>
         </div>
         </>
